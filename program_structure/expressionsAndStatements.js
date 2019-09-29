5b5b7534f1d693a5b6e05aff5b89ce108a9f4754
mattsharker <mattsharker@gmail.com>
2019-09-29 15:44:02 -0700

    Files contain information about program structure and control flow

---
+++ program_structure/expressionsAndStatements.js
@@ -0,0 +1,32 @@
+// STATEMENTS
+// most basic JS statement has a semicolon at the end of an expression
+7;
+!false;
+4 * 37;
+
+// semicolons can be omitted is certain scenarios, but it's safest to 
+    // always include them
+
+
+// BINDINGS
+// aka variables
+// a binding is an accessable piece of memory that stores information
+
+// LET
+// most basic version of creating a binding
+let ten = 5 + 5;
+ten = ten + 10;  // ten now stores 20
+
+// defining multiple bindings in one statement
+let one = 1, two = 2;
+
+
+// VAR
+// var (variable) works similarly to let, but has some different properties
+var name = "Matthew"
+// see ________ for more info
+
+
+// CONST
+// short for constant, the value in the binding cannot be changed once bound
+const hw = "Hello World!";
