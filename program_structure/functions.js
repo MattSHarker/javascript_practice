a73b086296243f46a03a1208a08d41b51ada50bf
mattsharker <mattsharker@gmail.com>
2019-10-03 12:18:19 -0700

    Updated name of a file being referenced

--- program_structure/functions.js
+++ program_structure/functions.js
@@ -8,4 +8,4 @@ console.log("the value of x is: ", x);  // prints the contents inside () to the 
 let max = Math.max(3, 6);   // this returns the higher of the two values 
                             // and it gets stored in the variable "max"
 
-// for creating functions, refer to functions/functions.js
+// for writing functions, refer to functions/declarations.js
