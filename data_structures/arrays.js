25131a3b842b025873dbbe5dda722b237ce7450e
mattsharker <mattsharker@gmail.com>
2019-10-03 23:25:26 -0700

    Initial version of files

---
+++ data_structures/arrays.js
@@ -0,0 +1,20 @@
+// CREATING ARRAYS
+let array = [1, 2, 3, 8, 16];
+
+
+// ACCESSING ELEMENTS
+// indexing starts at 0
+let one = array[0]; // one stores 1
+array[3] = 4;
+
+
+// ITERATING OVER ARRAYS
+
+// two ways to iterate
+for (let i = 0; i < array.length; i++)
+    ;   // do something
+
+for (let num in array)
+    ;   // do something
+
+
---
+++ data_structures/objects.js
@@ -0,0 +1,14 @@
+ // PROPERTIES
+
+ // expressions that access a property of a value
+ let arr = [1, 2, 3, 4, 5];
+
+ // the two way to access properties are . and []
+value.length;     // length is a propety of arr
+value[3];         // this is also a property of arr
+
+// All values have properties, except for null and undefined
+
+
+// METHODS
+
