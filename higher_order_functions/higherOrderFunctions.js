54bdb96345e9cabf7e1b74f21b3d6f110c93cf66
mattsharker <mattsharker@gmail.com>
2019-10-17 17:44:51 -0700

    Added more examples

--- higher_order_functions/higherOrderFunctions.js
+++ higher_order_functions/higherOrderFunctions.js
@@ -3,7 +3,7 @@
 
 
 // CREATING A FUNCTION WITH A FUNCTION
-const greaterThan = function(x) {
+const greaterThan = x => { // the outer function doesn't need to be an arrow decl.
     return y => y > x;
 };
 
@@ -24,3 +24,27 @@ let noisy = function(f)
 };
 
 noisy(Math.min)(3, 2, 1);
+// -> calling with [3, 2, 1]
+// -> called with [3, 2, 1] , returned 1
+
+
+// FUNCTIONS THAT PROVIDE CONTROL FLOW
+const unless = function(text, then)
+{
+    if (!text) then();
+}
+
+repeat(3, n =>
+{
+    unless(n%2 === 1, () =>
+    {
+        console.log(n, "is even"); 
+    }); 
+});
+// -> 0 is even
+// -> 2 is even
+
+// for each as a higher order function
+["A", "B"].forEach(l => console.log(l));
+// -> A
+// -> B
