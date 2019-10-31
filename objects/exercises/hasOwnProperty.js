297953155c6507e66a5613ad9f24a93291f6723e
mattsharker <mattsharker@gmail.com>
2019-10-31 13:43:32 -0700

    Actually remove the original renamed files

--- objects/exercises/hasOwnProperty.js
+++
@@ -1,9 +0,0 @@
-// original
-let map = {one: true, two: true, hasOwnProperty: true};
-
-// Fix this call
-console.log(map.hasOwnProperty("one")); // this will try to call the property and not the function
-// TypeError: map.hasOwnProperty is not a function (line 5 in function eval)
-
-// circumvent the property by calling the prototype itself
-console.log(Object.prototype.hasOwnProperty(map, "one"));
