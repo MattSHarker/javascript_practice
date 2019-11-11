cc3b0f8e208848dc8f76924bdcb4989f4643b569
mattsharker <mattsharker@gmail.com>
2019-11-11 00:06:56 -0800

    Details a few protocols used by the internet

---
+++ 13_the_browser/protocols.js
@@ -0,0 +1,46 @@
+// HTTP
+
+// Hyper Text Transfer Protocol
+// retrieves named sources (chunks of info)
+
+// Treats networks like a stream-like device where you can add
+    // bits and have them arrive at the desination in the right
+    // order
+
+
+// TCP
+
+// Transmission Control Protocol
+// One computer must be listening for other to talk to it
+    // In order to listen to multiple things at the same time,
+    // machines have different ports
+// Another computer establishes a connection by using the correct
+    // port number
+// If the target machine is reached and is listening on the connected
+    // port, a connection is created
+
+// The listener is called the "server" and the connector is the "client"
+
+// TCP is a two-way flow, both machines can send data
+
+
+// THE WEB
+
+// The World Wide Web (!=== the internet) is a set of protocols and formats
+// "Web" is an allusion to a web, how things are interconnected to each other
+// The port for the www is 80, and HTTP is used to connect to it
+
+
+// URL
+
+// Uniform Resource Locator
+// URL example: https://github.com/MattSHarker
+
+// The first part of the example (http://) shows the URL uses HTTP
+    // as opposed to Secure HTTP (https://)
+
+// The next part (github.com/) identifies which server we are sending
+    // requests to
+
+// The last part (MattSHarker) is the path that identifies which
+    // specific resource is being accessed
